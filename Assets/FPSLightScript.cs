0fff798ce9d123fff6fad60df396ef0d61e8d5c9
gabby-m056 <144246046+gabby-m056@users.noreply.github.com>
2026-01-16 17:32:35 +0000

    Chest

---
+++ Assets/FPSLightScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class FPSLightScript : MonoBehaviour
+{
+    public Light playerflashlight;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F)){
+            playerflashlight.enabled = !playerflashlight.enabled;
+        }
+    }
+}
