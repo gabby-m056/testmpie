e0c6ff02ca87237ea3f6e91c7fcd98578c7d4de1
gabby-m056 <144246046+gabby-m056@users.noreply.github.com>
2026-01-20 00:40:08 +0000

    Save

--- Assets/ButtonRestartScript.cs
+++ Assets/ButtonRestartScript.cs
@@ -5,11 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class ButtonRestartScript : MonoBehaviour
 {
-    public Button btn;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+   /* void Start()
     {
-        btn = gameObject.GetComponent<Button>();
+        Button btn = gameObject.GetComponent<Button>();
         btn.onClick.AddListener(ButtonIsClicked);
     }
 
@@ -17,10 +17,11 @@ public class ButtonRestartScript : MonoBehaviour
     void Update()
     {
         
-    }
+    }*/
 
     public void ButtonIsClicked()
     {
+        Debug.Log("ButtonClicked");
         //https://stackoverflow.com/questions/65851443/how-do-i-restart-the-scene-that-im-currently-in-through-script-in-unity-2d-so
         //https://docs.unity3d.com/6000.3/Documentation/ScriptReference/SceneManagement.SceneManager.GetSceneByName.html
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
