c012be2dcd83fce8ed5865a39836d56817e25e44
gabby-m056 <144246046+gabby-m056@users.noreply.github.com>
2026-01-22 14:49:37 +0000

    updated and commented chest script

--- Assets/sity/itens/chest/bauScript.cs
+++ Assets/sity/itens/chest/bauScript.cs
@@ -2,27 +2,42 @@
 using System.Collections;
 using System.Collections.Generic;
 
+/*
+	This script came with the chest asset that was inserted in my project
+
+	Author: Leandro Melchior (Publisher/User on Unity Asset Store)
+	Location: https://assetstore.unity.com/packages/3d/props/steampunk-chest-69723#content
+	Accessed: 16/01/2026
+*/
+//However there are various points that I have modified this script to suit the game which are clearly indicated
 public class bauScript : MonoBehaviour {
 
-
+	//I removed this variable from the original script as it was redundant data for my game
 	//public string player = "Player";
 	public float velocity = 10.0f;
+	bool abrir = false;
 
+	GameObject alvo;
+	bool cheio = true;
 	public GameObject target;
 	public List<GameObject> Contents;
-	//modification by me
+
+	//I added the following fields to link other game objects into this script
 	public GameObject player;
 	public GameObject key;
 	public GameObject pear;
 	public GameObject hintBox;
 
-	bool abrir = false;
-
-	GameObject alvo;
-	bool cheio = true;
+	//I added these fields for the script to be able to tell if various functions can happen
+	//bool for if the chest can be opened
 	bool canOpenChest=false;
+	//bool for if the pick up hint can be called
 	bool pickUpHintCalled = false;
+	//bool for if the game has started / instruction dialogue has finished
 	bool gameplayStarted;
+	//my modification ends
+
+	//these values were in the original script but are not used for this project
 	//rotatação max  -60 -900 -900
 	//0, -720, -720
 
@@ -33,9 +48,11 @@ public class bauScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//I added this so the script could get information on whether the game has started or not
 		gameplayStarted = player.GetComponent<PlayerHealthScript>().enabled;
 		if (gameplayStarted)
 		{
+			//if game has started and E is clicked chest can be opened
 				if(Input.GetKey(KeyCode.E))
 			{
 				canOpenChest=true;
@@ -45,7 +62,7 @@ public class bauScript : MonoBehaviour {
 				canOpenChest = false;
 			}
 
-			
+		//my modification ends	
 
 			if (abrir) {
 				
@@ -58,6 +75,7 @@ public class bauScript : MonoBehaviour {
 						
 					}
 				}
+				//I added this so once the chest has fully opened the pickup hint can be shown
 				if(this.transform.rotation.x <= -0.9 && pickUpHintCalled==false)
 				{
 					hintBox.SetActive(true);
@@ -65,10 +83,7 @@ public class bauScript : MonoBehaviour {
 					pickUpHintCalled = true;
 				}
 
-
-
-				
-				//modification by Me
+				//I added this so that once P was clicked the items were picked up, pear increases health and the hint is cleared
 
 				if (Input.GetKeyDown(KeyCode.P))
 				{
@@ -94,56 +109,63 @@ public class bauScript : MonoBehaviour {
 
 
 	void OnTriggerStay(Collider other) {
-
+		//I added these if statements so the script could tell if the gameplay has started
 		if (gameplayStarted)
 		{
-				if(other.gameObject.name == "FirstPersonController"&&abrir==false)
+			//and if the chest is closed when the player fires the trigger
+			if(other.gameObject.name == "FirstPersonController"&&abrir==false)
 			{
+			//My modification ends
 				if (alvo == null) {
-				Vector3 pos = this.transform.position;
-				pos.y += 0.5f;
-				pos.z += 0.25f;
-				alvo = Instantiate (target, pos, Quaternion.identity) as GameObject;
-				}
-
-				if (canOpenChest==true)
-				{
-						abrir = true;
-						Destroy (alvo.gameObject);
-				}
-				else
-				{
-					//enable hint script
+					Vector3 pos = this.transform.position;
+					pos.y += 0.5f;
+					pos.z += 0.25f;
+					alvo = Instantiate (target, pos, Quaternion.identity) as GameObject;
+					//I added these 2 lines of code to enable the open chest hint to appear
 					hintBox.SetActive(true);
 					hintBox.GetComponent<HintUpdateScript>().OpenChest();
+					//my modification ends
 				}
+				// I added this if statement to check if the chest can open - i.e the E key is pressed
+				if (canOpenChest)
+				{
+				//my modification ends
+					abrir = true;
+					Destroy (alvo.gameObject);
+					//I added this line of code to clear and hide the hintbox
+					hintBox.GetComponent<HintUpdateScript>().ClearHint();
+				}
+				
+				
 			}
 		}
-
-		
-		
-
-
-
+		//my modification ends
 	}
 
 	void OnTriggerExit(Collider other) {
 
 		
-		
+		//I added this if statement so the script could tell if the gameplay has started
 		if (gameplayStarted)
 		{
+			//I added this line of code to clear and hide the hintbox
 			hintBox.GetComponent<HintUpdateScript>().ClearHint();
+			//my modification ends
+
 			Destroy (alvo.gameObject);
+			//I added this line of code to set the ability to open chest to false
 			canOpenChest = false;
 		}
-		
+		//my modification ends
 		
 	}
 
 
 	public void liberar(){
+		//I added this line of code to clear and hide the hintbox
 		hintBox.GetComponent<HintUpdateScript>().ClearHint();
+		//my modification ends
+
 		Vector3 pos = this.transform.position;
 		//pos.x = 0;
 		pos.y += 0.4f;
@@ -158,9 +180,6 @@ public class bauScript : MonoBehaviour {
 			}
 			rb.AddForce (new Vector3(0,1,1));
 		}
-
-
-
 	}
 
 }	
